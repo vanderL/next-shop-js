2525c44b836012e553b6d9dc0fd30a43aa2731e0
Vander Lima <vnd.vander@gmail.com>
2023-03-21 01:12:25 -0300

    feat: Config Stitches styles

---
+++ src/styles/index.ts
@@ -0,0 +1,9 @@
+import { createStitches } from "@stitches/react";
+
+export const { config, createTheme, styled, globalCss, getCssText, keyframes, css, theme } = createStitches({
+  theme: {
+    colors: {
+      banana: '#8257e6'
+    }
+  }
+})
