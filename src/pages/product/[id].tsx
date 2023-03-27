52fb47710175ea6b1ce2a5b5944a7e72fc43f57f
Vander Lima <vnd.vander@gmail.com>
2023-03-27 01:36:07 -0300

    feat: Checkout session stripe

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -12,16 +12,21 @@ interface ProductProps {
     imageUrl: string,
     price: string,
     description: string,
+    defaultPriceId: string,
   }
 }
 
 
-export default function ProductComId({ product }: ProductProps) {
+export default function ProductWithId({ product }: ProductProps) {
   // const { isFallback } = useRouter()
 
   // if (isFallback) {
   //   return <p>Loading...</p>
   // }
+  function handleBuyButton() {
+    console.log(product.defaultPriceId);
+
+  }
 
   return (
     <ProductContainer>
@@ -33,7 +38,7 @@ export default function ProductComId({ product }: ProductProps) {
         <h1>{product.name}</h1>
         <span>{product.price}</span>
         <p>{product.description}</p>
-        <button>Comprar agora</button>
+        <button onClick={handleBuyButton}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   )
@@ -68,6 +73,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           currency: 'BRL',
         }).format(price.unit_amount / 100),
         description: product.description,
+        defaultPriceId: price.id
       }
     },
     revalidate: 60 * 60 * 1,
